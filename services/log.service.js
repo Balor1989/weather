5acc6a889246e6dfc5fd5716c92f7c3aa8e8220f
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-05-14 17:19:10 +0300

    adds healpers and services

---
+++ services/log.service.js
@@ -0,0 +1,23 @@
+import chalk from "chalk";
+import dedent from "dedent";
+
+const printError = (error) => {
+  console.log(chalk.bgRed("Error:")` ${error}`);
+};
+
+const printSuccess = (message) => {
+  console.log(chalk.bgGreen("Success:")` ${message}`);
+};
+
+const printHelp = () => {
+  console.log(
+    dedent(`${chalk.bgMagenta("Help")}
+         Без параметров - вывод погоды
+         -s [CITY] - установка города
+         -р - вывод помощи
+         -t [API_KEY] - сохранение токена
+        `)
+  );
+};
+
+export { printError, printSuccess, printHelp };
